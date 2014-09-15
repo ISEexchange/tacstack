65e2b426da648358882ddc7f8fe84a6649e8fca9
Carlos Bautista <cbautista@ise.com>
2014-09-15 17:39:41 -0400

    Initial [skip build]

---
+++ src/files/bdt_map_setup.sh
@@ -0,0 +1,5 @@
+cd /home
+svn checkout https://v-db-svn01.office.iseoptions.com/svn/repos/ISE/ASGScripts/TestAutomation/trunk/BDT_MAP SVN_BDT_MAP
+
+mkdir /var/www/html/map
+ln -s /home/SVN_BDT_MAP/map_frontend_web_gui/* /var/www/html/map*
