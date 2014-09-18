e4b740e0609da0f07aa76799a3bcd1ff12fe72a4
Carlos Bautista <cbautista@ise.com>
2014-09-18 16:55:22 -0400

    Updated svn path to account for proper svn directory structure [skip build]
    BDT_MAP project now contains branches, tags and trunk

--- src/files/bdt_map_setup.sh
+++ src/files/bdt_map_setup.sh
@@ -2,4 +2,4 @@ cd /home
 svn checkout https://v-db-svn01.office.iseoptions.com/svn/repos/ISE/ASGScripts/TestAutomation/trunk/BDT_MAP SVN_BDT_MAP
 
 mkdir /var/www/html/map
-ln -s /home/SVN_BDT_MAP/map_frontend_web_gui/* /var/www/html/map*
+ln -s /home/SVN_BDT_MAP/trunk/map_frontend_web_gui/* /var/www/html/map*
