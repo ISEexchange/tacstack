04957aa84a053dd5ab209ee6b7d7a1f30997694d
Carlos Bautista <cbautista@ise.com>
2014-11-10 13:28:53 -0500

    Add Robot SVN checkout; Update SVN paths

--- src/files/bdt_map_setup.sh
+++ src/files/bdt_map_setup.sh
@@ -1,5 +1,7 @@
 cd /home
-svn checkout https://v-db-svn01.office.iseoptions.com/svn/repos/ISE/ASGScripts/TestAutomation/trunk/BDT_MAP SVN_BDT_MAP
-
 mkdir /var/www/html/map
-ln -s /home/SVN_BDT_MAP/trunk/map_frontend_web_gui/* /var/www/html/map*
+
+svn checkout https://v-db-svn01.office.iseoptions.com/svn/repos/ISE/ASGScripts/TestAutomation/BDT_MAP/trunk /home/SVN_BDT_MAP
+svn checkout https://v-db-svn01.office.iseoptions.com/svn/repos/ISE/ASGScripts/TestAutomation/trunk/RobotFramework /home/SVN_RobotFramework
+
+ln -s /home/SVN_BDT_MAP/map_frontend_web_gui/* /var/www/html/map*
