9f8374e811c614fd9e445d9f0e6a108f4f75ab35
Carlos Bautista <cbautista@ise.com>
2014-12-07 23:06:21 +0000

    Fix checkout directory

--- src/files/bdt_map_setup.sh
+++ src/files/bdt_map_setup.sh
@@ -1,13 +1,14 @@
-cd /home
+# Make directories needed for viewing site
 mkdir /var/www/html/map
 mkdir /var/www/html/map/results
 
-svn checkout https://v-db-svn01.office.iseoptions.com/svn/repos/ISE/ASGScripts/TestAutomation/BDT_MAP/trunk /home/SVN_BDT_MAP
+# Perform SVN Checkouts
+svn checkout https://v-db-svn01.office.iseoptions.com/svn/repos/ISE/ASGScripts/TestAutomation/BDT_MAP              /home/SVN_BDT_MAP
 svn checkout https://v-db-svn01.office.iseoptions.com/svn/repos/ISE/ASGScripts/TestAutomation/trunk/RobotFramework /home/SVN_RobotFramework
 
+#Link checkouts with site path
 ln -s /home/SVN_BDT_MAP/map_frontend_web_gui/* /var/www/html/map*
 ln -s /var/ftp/pub/uploads /var/www/html/map/results
-
 
 # Steps for deployment
 mkdir /home/SVC_UC4
