1798449b2686288983c5a8d7144570fe147595f5
Carlos Bautista <cbautista@ise.com>
2014-12-07 22:45:12 +0000

    Add ssh config so that prompts are not shown on ssh runs

--- src/files/bdt_map_setup.sh
+++ src/files/bdt_map_setup.sh
@@ -7,3 +7,9 @@ svn checkout https://v-db-svn01.office.iseoptions.com/svn/repos/ISE/ASGScripts/T
 
 ln -s /home/SVN_BDT_MAP/map_frontend_web_gui/* /var/www/html/map*
 ln -s /var/ftp/pub/uploads /var/www/html/map/results
+
+
+# Steps for deployment
+mkdir /home/SVC_UC4
+mkdir /home/SVC_UC4/.ssh
+mv    /home/config /home/SVC_UC4/.ssh/
